0c0f229228609e02a932384dbf8bf483c87afa9e
Hyeryeon-Seo <0168968060@naver.com>
2024-02-21 03:47:12 +0900

    update : route 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "pages/Home"; // pages/Home 절대경로설정됨
 import Detail from "pages/Detail";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
 
 const Router = () => {
 	return (
@@ -10,6 +12,11 @@ const Router = () => {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="detail/:id" element={<Detail />} />
+					<Route path="login" element={<Login />} />
+					<Route path="profile/:userId" element={<Profile />} />
+					{/* 개개인 식별가능한 id를 param으로 */}
+					<Route path="*" element={<Navigate replace to="/" />} />
+					{/*그 외 다른 path name이 url에 온 경우(*) 홈으로 이동(redirect)시키기 : Navigate사용 */}
 				</Routes>
 			</BrowserRouter>
 		</>
