85f57fd8afc5be123074827287e2bd2163e5599e
Hyeryeon-Seo <0168968060@naver.com>
2024-02-20 23:16:13 +0900

    update : 헤더 수정 (메뉴추가)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Header() {
 	return (
 		<header>
-			<h1>Send 💌 your love 💌 to aespa</h1>
+			<h1>Send 💌 Your Love 💌 to &nbsp; a e s p a</h1>
 			{/* TODO 시간 뜨게 하기? */}
+			<HeaderTextBox>
+				<HeaderLink Link to="profile">
+					{/* id 넣은 프로필주소로 변경하기 */}내 프로필
+				</HeaderLink>
+				<HeaderLink Link to="login">
+					로그아웃
+				</HeaderLink>
+			</HeaderTextBox>
 		</header>
 	);
 }
 
 export default Header;
+
+const HeaderTextBox = styled.div`
+	display: flex;
+	flex-direction: row;
+	gap: 30px;
+	margin-right: 30px;
+`;
+
+const HeaderLink = styled(Link)`
+	text-decoration: none;
+	font-family: "Pretendard-Regular";
+	font-weight: bold;
+	font-size: 18px;
+	color: black;
+	&:hover {
+		color: var(--subcolor);
+		transition: 0.3s;
+	}
+`;
