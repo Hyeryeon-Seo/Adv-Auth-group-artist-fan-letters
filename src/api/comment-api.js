6de6bbbb37902105d2094631b8c5d7c71d9e7dcd
Hyeryeon-Seo <0168968060@naver.com>
2024-02-21 16:35:29 +0900

    feat : comment-api 추가 (comment - json-server db 활용위해)

---
+++ src/api/comment-api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const commentClient = axios.create({
+	baseURL: "http://localhost:5000/comments",
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+
+// 외부서버- json-server db에서 조회-가져오고, post 넣고 등등 하기
+// redux-Slice 모듈의 set..state와 다름
+
+const getComments = async () => {
+	const { data } = await commentClient.get("/");
+	// console.log(data);
+	return data;
+};
+
+const getSingleComment = async (id) => {
+	const { data } = await commentClient.get(`/${id}`); //?
+	return data;
+};
+
+const createComment = async (comment) => {
+	await commentClient.post("/", comment);
+};
+
+const deleteComment = async (id) => {
+	await commentClient.delete(`/${id}`);
+};
+
+const updateComment = async (id, comment) => {
+	await commentClient.patch(`/${id}`, comment);
+};
+
+export {
+	getComments,
+	getSingleComment,
+	createComment,
+	deleteComment,
+	updateComment,
+};
