34e3581cc05bea67016a1cd891dd65d60737ba8a
Hyeryeon-Seo <0168968060@naver.com>
2024-02-21 16:57:13 +0900

    add, update : .env 파일 추가 및 comment-api에서 .env의 url 변수 사용

--- src/api/comment-api.js
+++ src/api/comment-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const commentClient = axios.create({
-	baseURL: "http://localhost:5000/comments",
+	baseURL: process.env.REACT_APP_SERVER_URL,
 	headers: {
 		"Content-Type": "application/json",
 	},
